1b87c3f5e170d56c18bef6c3e6b811ac7cc9e404
Ari-Schrier <schrier.a@northeastern.edu>
2025-04-23 22:04:28 -0700

    Entities created by entity_factories

---
+++ entity_factories.py
@@ -0,0 +1,5 @@
+from entity import Entity
+player = Entity(char="@", color=(255, 255, 255), name="Player", blocks_movement=True)
+
+carnivalImp = Entity(char="i", color=(63, 127, 63), name="Carnie Imp", blocks_movement=True)
+circusDemon = Entity(char="D", color=(127, 0, 0), name="Circus Demon", blocks_movement=True)
